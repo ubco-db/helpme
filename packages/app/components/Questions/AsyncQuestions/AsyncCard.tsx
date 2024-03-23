3ba0ad264462fc899bb1b0549aa76de183428cba
Ferdinand <ferdinandhaaben@proton.me>
2024-03-23 09:10:26 -0700

    added voting back

--- packages/app/components/Questions/AsyncQuestions/AsyncCard.tsx
+++ packages/app/components/Questions/AsyncQuestions/AsyncCard.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement, useState } from 'react'
-import { Button, Card, message } from 'antd'
+import React, { ReactElement, useEffect, useState } from 'react'
+import { Button, Card, Image, message } from 'antd'
 import { Text } from '../Shared/SharedComponents'
 import { QuestionType } from '../Shared/QuestionType'
 import { KOHAvatar } from '../../common/SelfAvatar'
@@ -9,6 +9,7 @@ import { AsyncQuestion, asyncQuestionStatus } from '@koh/common'
 import StudentQuestionDetailButtons from './StudentQuestionDetailButtons'
 import { API } from '@koh/api-client'
 import styled, { keyframes, css } from 'styled-components'
+import { DownOutlined, UpOutlined } from '@ant-design/icons'
 
 const flashAnimation = keyframes`
   0%, 100% { opacity: 1; }
@@ -39,8 +40,6 @@ const statusDisplayMap = {
 
 interface AsyncCardProps {
   question: AsyncQuestion
-  cid: number
-  qid: number
   isStaff: boolean
   userId: number
   onStatusChange: () => void
@@ -55,6 +54,8 @@ export default function AsyncCard({
   onQuestionTypeClick,
 }: AsyncCardProps): ReactElement {
   const [isExpanded, setIsExpanded] = useState(false)
+  const [voteCount, setVoteCount] = useState(question.votesSum)
+  const [thisUserThisQuestionVote, setThisUserThisQuestionVote] = useState()
   const shouldFlash =
     question.status === asyncQuestionStatus.AIAnswered &&
     userId === question.creatorId
@@ -82,9 +83,27 @@ export default function AsyncCard({
     }
   }
 
+  const handleVote = async (questionId: number, vote: number) => {
+    const resp = await API.asyncQuestions.vote(questionId, vote)
+    setVoteCount(resp.questionSumVotes)
+  }
+
+  useEffect(() => {
+    async function getVoteForuser() {
+      const resp = await API.asyncQuestions.vote(question.id, 0)
+      setThisUserThisQuestionVote(resp.vote)
+    }
+    if (question) {
+      getVoteForuser()
+    }
+  }, [voteCount])
+
+  const upVoteStyle = thisUserThisQuestionVote === 1 ? { color: 'green' } : {}
+  const downVoteStyle = thisUserThisQuestionVote === -1 ? { color: 'red' } : {}
+
   return (
-    <Card
-      className={`mb-2 rounded-lg bg-white p-2 shadow-lg ${
+    <div
+      className={`mb-2 flex rounded-lg bg-white p-2 shadow-lg ${
         question.status === asyncQuestionStatus.HumanAnswered ||
         question.status === asyncQuestionStatus.AIAnsweredResolved
           ? 'bg-green-100/50'
@@ -92,102 +111,122 @@ export default function AsyncCard({
       }`}
       onClick={() => setIsExpanded(!isExpanded)}
     >
-      <div className="mb-4 flex items-start justify-between">
-        {isStaff || userId == question.creatorId ? (
-          <>
-            {/* Staff can see all questions details, question creator can see their own */}
-            <KOHAvatar
-              size={46}
-              name={question.creator.name}
-              photoURL={question.creator.photoURL}
-              className="mr-3" // Tailwind margin right
-            />
-            <div className="flex-grow text-sm italic">
-              {question.creator.name}
-            </div>
-          </>
-        ) : (
-          <div className="flex-grow text-sm italic">Anonymous Student</div>
-        )}
-        <div
-          className={`flex flex-grow items-center justify-center rounded-full px-2 py-1 
-    ${
-      question.status === asyncQuestionStatus.HumanAnswered
-        ? 'bg-green-200'
-        : 'bg-yellow-200'
-    }`}
-        >
-          {question.status === asyncQuestionStatus.HumanAnswered
-            ? `Verified by ${question.taHelped.firstName} ${question.taHelped.lastName}`
-            : statusDisplayMap[question.status]}
-        </div>
-        <div className="flex items-center">
-          <Text className="text-sm">{getAsyncWaitTime(question)}</Text>
-          {/*Stuff gets buttons to edit and delete questions*/}
-          {isStaff && (
-            <>
-              <TAquestionDetailButtons
-                question={question}
-                setIsExpandedTrue={setIsExpandedTrue}
-                onStatusChange={onStatusChange}
-              />
-            </>
-          )}
-          {userId === question.creatorId &&
-          question.status === asyncQuestionStatus.AIAnswered ? (
-            <>
-              {/* Students can edit their own questions, but only if question is not resolved, note that AIAnswer is default */}
-              <StudentQuestionDetailButtons
-                question={question}
-                setIsExpandedTrue={setIsExpandedTrue}
-                onStatusChange={onStatusChange}
-              />
-            </>
-          ) : (
-            <></>
-          )}
-        </div>
+      <div className="mr-4 flex flex-col items-center justify-center">
+        <Button
+          type="text"
+          icon={<UpOutlined style={upVoteStyle} />}
+          onClick={(e) => {
+            e.stopPropagation() // Prevent card expansion
+            handleVote(question.id, 1)
+          }}
+        />
+        <div className="my-2 flex items-center justify-center">{voteCount}</div>
+        <Button
+          type="text"
+          icon={<DownOutlined style={downVoteStyle} />}
+          onClick={(e) => {
+            e.stopPropagation() // Prevent card expansion
+            handleVote(question.id, -1)
+          }}
+        />
       </div>
-      <div className="mb-4">
-        <h4 className="font-bold">{question.questionAbstract}</h4>
-        {isExpanded && (
-          <div>
-            {question.questionText && <Text>{question.questionText}</Text>}
 
-            {question.answerText ? (
+      <div className="flex w-full flex-grow flex-col">
+        <div className="mb-4">
+          <div className="justify between flex items-start">
+            {isStaff || userId == question.creatorId ? (
               <>
-                <br />
-                <div>
-                  <strong>Answer</strong>
-                  <Text>{question.answerText}</Text>
+                <KOHAvatar
+                  size={46}
+                  name={question.creator.name}
+                  photoURL={question.creator.photoURL}
+                  className="mr-3" // Tailwind margin right
+                />
+                <div className="flex-grow text-sm italic">
+                  {question.creator.name}
                 </div>
               </>
             ) : (
-              <></>
+              <div className="flex-grow text-sm italic">Anonymous Student</div>
+            )}
+            <div
+              className={`flex flex-grow items-center justify-center rounded-full px-2 py-1 
+          ${
+            question.status === asyncQuestionStatus.HumanAnswered
+              ? 'bg-green-200'
+              : 'bg-yellow-200'
+          }`}
+            >
+              {statusDisplayMap[question.status]}
+            </div>
+            <div className="flex items-center">
+              <Text className="text-sm">{getAsyncWaitTime(question)}</Text>
+              {isStaff && (
+                <>
+                  <TAquestionDetailButtons
+                    question={question}
+                    hasUnresolvedRephraseAlert={false}
+                    setIsExpandedTrue={setIsExpandedTrue}
+                  />
+                </>
+              )}
+              {userId === question.creatorId &&
+              question.status === asyncQuestionStatus.AIAnswered ? (
+                <>
+                  {/* Students can edit their own questions, but only if question is not resolved, note that AIAnswer is default */}
+                  <StudentQuestionDetailButtons
+                    question={question}
+                    setIsExpandedTrue={setIsExpandedTrue}
+                    onStatusChange={onStatusChange}
+                  />
+                </>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+          <div>
+            <h4 className="font-bold">{question.questionAbstract}</h4>
+            {isExpanded && (
+              <div>
+                {question.questionText && <Text>{question.questionText}</Text>}
+
+                {question.answerText ? (
+                  <>
+                    <br />
+                    <div>
+                      <strong>Answer:</strong>
+                      <Text>{question.answerText}</Text>
+                    </div>
+                  </>
+                ) : (
+                  <></>
+                )}
+              </div>
             )}
           </div>
-        )}
+          <div className="flex flex-wrap">
+            {question.questionTypes?.map((questionType, index) => (
+              <QuestionType
+                key={index}
+                typeName={questionType.name}
+                typeColor={questionType.color}
+                onClick={() => onQuestionTypeClick(questionType.id)}
+              />
+            ))}
+          </div>
+        </div>
+        {question.status === asyncQuestionStatus.AIAnswered &&
+          userId === question.creatorId && (
+            <FlashingSection shouldFlash={shouldFlash}>
+              {/* Students vote on whether they still need faculty help */}
+              <Button onClick={() => handleFeedback(true)}>Satisfied</Button>
+              <Button type="primary" onClick={() => handleFeedback(false)}>
+                Still need faculty Help
+              </Button>
+            </FlashingSection>
+          )}
       </div>
-      <div className="flex flex-wrap">
-        {question.questionTypes?.map((questionType, index) => (
-          <QuestionType
-            key={index}
-            typeName={questionType.name}
-            typeColor={questionType.color}
-            onClick={() => onQuestionTypeClick(questionType.id)}
-          />
-        ))}
-      </div>
-      {question.status === asyncQuestionStatus.AIAnswered &&
-        userId === question.creatorId && (
-          <FlashingSection shouldFlash={shouldFlash}>
-            {/* Students vote on whether they still need faculty help */}
-            <Button onClick={() => handleFeedback(true)}>Satisfied</Button>
-            <Button type="primary" onClick={() => handleFeedback(false)}>
-              Still need faculty Help
-            </Button>
-          </FlashingSection>
-        )}
-    </Card>
+    </div>
   )
 }
