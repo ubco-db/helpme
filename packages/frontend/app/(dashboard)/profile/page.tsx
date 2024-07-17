89d340aa2b83b96471b0a184241f21a2f269e4bf
Dima Zhuravel <dmitrozhyravel@gmail.com>
2024-07-16 21:15:06 -0700

    temp commit component

---
+++ packages/frontend/app/(dashboard)/profile/page.tsx
@@ -0,0 +1,16 @@
+import { ReactElement } from 'react'
+
+export default function ProfilePage(): ReactElement {
+  return (
+    <div className="mt-8">
+      <div className="header">
+        <h2 className="mb-2">Settings</h2>
+        <hr />
+      </div>
+
+      {/* <Panel>
+
+      </Panel> */}
+    </div>
+  )
+}
