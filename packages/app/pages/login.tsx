af368669d9cbe61913a8a418235a54bd6650fd3c
Adam Fipke <adamfipke@gmail.com>
2024-07-17 13:12:11 -0700

    switch bavck to Router.push with login with google

--- packages/app/pages/login.tsx
+++ packages/app/pages/login.tsx
@@ -44,7 +44,7 @@ export default function Login(): ReactElement {
     await API.auth
       .loginWithGoogle(Number(organization.id))
       .then((res) => {
-        window.location.href = res.redirectUri
+        Router.push(res.redirectUri)
       })
       .catch((err) => {
         console.log(err)
