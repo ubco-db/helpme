a190631aa20e9f87f3c56cb66061e5667aa7ce69
wskksw <66136750+wskksw@users.noreply.github.com>
2024-07-26 23:13:26 -0700

    chatbot questions refactor

--- packages/app/components/ChatbotSettings/ChatbotQuestions.tsx
+++ packages/app/components/ChatbotSettings/ChatbotQuestions.tsx
@@ -1,25 +1,29 @@
-import { Button, Form, Input, Modal, Select, Switch, Table } from 'antd'
+import { Button, Form, Input, Modal, Table } from 'antd'
 import React, { ReactElement, useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import ExpandableText from '../common/ExpandableText'
-import EditChatbotQuestionModal from '../Settings/EditChatbotQuestionModal'
 import { useProfile } from '../../hooks/useProfile'
-import { filter } from 'lodash'
+import AddQuestionModal from './ChatbotSettingModals/AddQuestionModal'
+import EditQuestionModal from './ChatbotSettingModals/EditQuestionModal'
 
 interface Loc {
   pageNumber: number
 }
 
 interface SourceDocument {
-  id: string
-  metadata: {
-    loc: Loc
+  id?: string
+  metadata?: {
+    loc?: Loc
     name: string
-    type: string
-    source: string
-    courseId: string
+    type?: string
+    source?: string
+    courseId?: string
   }
-  pageContent: string
+  type?: string
+  content: string
+  docName: string
+  sourceLink?: string
+  pageNumber?: number
 }
 
 interface IncomingQuestionData {
@@ -41,9 +45,11 @@ interface ChatbotQuestion {
   sourceDocuments: SourceDocument[]
   suggested: boolean
 }
+
 type ChatbotQuestionsProps = {
   courseId: number
 }
+
 export default function ChatbotQuestions({
   courseId,
 }: ChatbotQuestionsProps): ReactElement {
@@ -58,7 +64,6 @@ export default function ChatbotQuestions({
   const [editRecordModalVisible, setEditRecordModalVisible] = useState(false)
   const [chatQuestions, setChatQuestions] = useState<ChatbotQuestion[]>([])
   const [existingDocuments, setExistingDocuments] = useState([])
-  const [selectedDocuments, setSelectedDocuments] = useState([])
 
   useEffect(() => {
     if (courseId) {
@@ -71,8 +76,8 @@ export default function ChatbotQuestions({
           const formattedDocuments = json.map((doc) => ({
             docId: doc.id,
             docName: doc.pageContent,
-            sourceLink: doc.metadata.source,
-            pageNumbers: [],
+            sourceLink: doc.metadata?.source || '', // Handle the optional source field
+            pageNumbers: doc.metadata?.loc ? [doc.metadata.loc.pageNumber] : [], // Handle the optional loc field
           }))
           setExistingDocuments(formattedDocuments)
         })
@@ -111,23 +116,32 @@ export default function ChatbotQuestions({
                 key={index}
               >
                 <div className="truncate font-semibold">
-                  <a
-                    href={doc.sourceLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {doc.docName}
-                  </a>
+                  {doc.sourceLink ? (
+                    <a
+                      href={doc.sourceLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {doc.docName}
+                    </a>
+                  ) : (
+                    <span>{doc.docName}</span>
+                  )}
                 </div>
-                <div className="mt-1 flex flex-wrap gap-1 text-xs">
-                  {doc.pageNumbers.map((pageNumber) => (
-                    <span
-                      key={`${doc.docName}-${pageNumber}`}
+                <div className="mt-1 flex  gap-1 text-xs">
+                  {doc.pageNumber ? (
+                    <div
+                      key={`${doc.docName}-${doc.pageNumber}`}
                       className="whitespace-nowrap"
                     >
-                      p.{pageNumber}
-                    </span>
-                  ))}
+                      p.{doc.pageNumber}
+                    </div>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <div className="mt-1 flex flex-wrap gap-1 text-xs">
+                  {doc.type ? <></> : <span>{doc.content}</span>}
                 </div>
               </div>
             ))}
@@ -147,9 +161,7 @@ export default function ChatbotQuestions({
       onFilter: (value, record) => record.verified === value,
       render: (verified) => (
         <span
-          className={`rounded px-2 py-1 ${
-            verified ? 'bg-green-100' : 'bg-red-100'
-          }`}
+          className={`rounded px-2 py-1 ${verified ? 'bg-green-100' : 'bg-red-100'}`}
         >
           {verified ? 'Verified' : 'Unverified'}
         </span>
@@ -167,9 +179,7 @@ export default function ChatbotQuestions({
       onFilter: (value, record) => record.suggested === value,
       render: (suggested) => (
         <span
-          className={`rounded px-2 py-1 ${
-            suggested ? 'bg-green-100' : 'bg-red-100'
-          }`}
+          className={`rounded px-2 py-1 ${suggested ? 'bg-green-100' : 'bg-red-100'}`}
         >
           {suggested ? 'Yes' : 'No'}
         </span>
@@ -180,7 +190,7 @@ export default function ChatbotQuestions({
       dataIndex: 'edit',
       key: 'edit',
       render: (_, record) => (
-        <Button onClick={() => showModal(record)}>Edit</Button>
+        <Button onClick={() => showEditModal(record)}>Edit</Button>
       ),
     },
     {
@@ -212,7 +222,7 @@ export default function ChatbotQuestions({
     getQuestions()
   }, [editingRecord, getQuestions])
 
-  const showModal = (record) => {
+  const showEditModal = (record) => {
     setEditingRecord(record)
     setEditRecordModalVisible(true)
   }
@@ -249,51 +259,13 @@ export default function ChatbotQuestions({
     }
   }
 
-  const addQuestion = async () => {
-    const formData = await form.validateFields()
-
-    try {
-      selectedDocuments.forEach((doc) => {
-        if (typeof doc.pageNumbers === 'string') {
-          // Convert string to array of numbers, trimming spaces and ignoring empty entries
-          doc.pageNumbers = doc.pageNumbers
-            .split(',')
-            .map((page) => page.trim())
-            .filter((page) => page !== '')
-            .map((page) => parseInt(page, 10))
-        }
-      })
-
-      await fetch(`/chat/${courseId}/question`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          question: formData.questionText,
-          answer: formData.responseText,
-          verified: formData.verified,
-          suggested: formData.suggested,
-          sourceDocuments: selectedDocuments,
-        }),
-      })
-
-      getQuestions()
-      setAddModelOpen(false)
-      toast.success('Question added.')
-    } catch (e) {
-      toast.error('Failed to add question.' + e)
-    } finally {
-      form.resetFields()
-    }
-  }
-
   const deleteQuestion = async (questionId) => {
     try {
       await fetch(`/chat/${courseId}/question/${questionId}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
+          HMS_API_TOKEN: profile.chat_token.token,
         },
       })
 
@@ -306,114 +278,19 @@ export default function ChatbotQuestions({
 
   return (
     <div className="m-auto my-5 max-w-[1000px]">
-      <Modal
-        title="Create a new question for your students!"
-        open={addModelOpen}
-        onCancel={() => setAddModelOpen(false)}
-        footer={[
-          <Button key="ok" type="ghost" onClick={() => setAddModelOpen(false)}>
-            Cancel
-          </Button>,
-          <Button key="ok" type="primary" onClick={addQuestion}>
-            Submit
-          </Button>,
-        ]}
-      >
-        <Form form={form}>
-          <Form.Item
-            label="Question"
-            name="questionText"
-            rules={[
-              {
-                required: true,
-                message: 'Please input a question!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Answer"
-            name="responseText"
-            rules={[
-              {
-                required: true,
-                message: 'Please input an answer!',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            label="Verified"
-            name="verified"
-            valuePropName="checked"
-            initialValue={true}
-          >
-            <Switch />
-          </Form.Item>
-          <Form.Item
-            label="Suggested"
-            name="suggested"
-            valuePropName="checked"
-            initialValue={false}
-          >
-            <Switch />
-          </Form.Item>
-          <Select
-            className="my-4"
-            placeholder="Select a document to add"
-            style={{ width: '100%' }}
-            onSelect={(selectedDocId) => {
-              const selectedDoc = existingDocuments.find(
-                (doc) => doc.docId === selectedDocId,
-              )
-              if (selectedDoc) {
-                setSelectedDocuments((prev) => {
-                  const isAlreadySelected = prev.some(
-                    (doc) => doc.docId === selectedDocId,
-                  )
-                  if (!isAlreadySelected) {
-                    return [...prev, { ...selectedDoc, pageNumbers: [] }]
-                  }
-                  return prev
-                })
-              }
-            }}
-          >
-            {existingDocuments.map((doc) => (
-              <Select.Option key={doc.docId} value={doc.docId}>
-                {doc.docName}
-              </Select.Option>
-            ))}
-          </Select>
-
-          {selectedDocuments.map((doc, index) => (
-            <div key={doc.docId}>
-              <span className="font-bold">{doc.docName}</span>
-              <Input
-                type="text"
-                placeholder="Enter page numbers (comma separated)"
-                value={doc.pageNumbers}
-                onChange={(e) => {
-                  const updatedPageNumbers = e.target.value
-                  // Split by comma, trim whitespace, filter empty strings, convert to numbers
-                  const pageNumbersArray = updatedPageNumbers
-                    .split(',')
-                    .map(Number)
-                  setSelectedDocuments((prev) =>
-                    prev.map((d, idx) =>
-                      idx === index
-                        ? { ...d, pageNumbers: pageNumbersArray } // array of numbers
-                        : d,
-                    ),
-                  )
-                }}
-              />
-            </div>
-          ))}
-        </Form>
-      </Modal>
+      <AddQuestionModal
+        visible={addModelOpen}
+        onClose={() => setAddModelOpen(false)}
+        courseId={courseId}
+        existingDocuments={existingDocuments}
+        getQuestions={getQuestions}
+      />
+      <EditQuestionModal
+        visible={editRecordModalVisible}
+        setEditingRecord={setEditRecordModalVisible}
+        editingRecord={editingRecord}
+        onSuccessfulUpdate={getQuestions}
+      />
       <div className="flex w-full items-center justify-between">
         <div className="">
           <h3 className="m-0 p-0 text-4xl font-bold text-gray-900">
@@ -428,7 +305,6 @@ export default function ChatbotQuestions({
       <hr className="my-5 w-full"></hr>
       <Input
         placeholder={'Search question...'}
-        // prefix={<SearchOutlined />}
         value={search}
         onChange={(e) => {
           e.preventDefault()
@@ -441,13 +317,6 @@ export default function ChatbotQuestions({
         dataSource={filteredQuestions}
         style={{ maxWidth: '1000px' }}
         pagination={{ pageSize: 7 }}
-      />
-
-      <EditChatbotQuestionModal
-        editingRecord={editingRecord}
-        visible={editRecordModalVisible}
-        setEditingRecord={setEditRecordModalVisible}
-        onSuccessfulUpdate={getQuestions}
       />
     </div>
   )
---
+++ packages/app/components/ChatbotSettings/ChatbotSettingModals/AddQuestionModal.tsx
@@ -0,0 +1,171 @@
+import React, { useState } from 'react'
+import { Button, Form, Input, Modal, Select, Switch, message } from 'antd'
+
+interface AddQuestionModalProps {
+  visible: boolean
+  onClose: () => void
+  courseId: number
+  existingDocuments: any[]
+  getQuestions: () => void
+}
+
+export default function AddQuestionModal({
+  visible,
+  onClose,
+  courseId,
+  existingDocuments,
+  getQuestions,
+}: AddQuestionModalProps): React.ReactElement {
+  const [form] = Form.useForm()
+  const [selectedDocuments, setSelectedDocuments] = useState([])
+
+  const addQuestion = async () => {
+    const formData = await form.validateFields()
+
+    try {
+      selectedDocuments.forEach((doc) => {
+        if (typeof doc.pageNumbers === 'string') {
+          // Convert string to array of numbers, trimming spaces and ignoring empty entries
+          doc.pageNumbers = doc.pageNumbers
+            .split(',')
+            .map((page) => page.trim())
+            .filter((page) => page !== '')
+            .map((page) => parseInt(page, 10))
+        }
+      })
+
+      await fetch(`/chat/${courseId}/question`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          question: formData.questionText,
+          answer: formData.responseText,
+          verified: formData.verified,
+          suggested: formData.suggested,
+          sourceDocuments: selectedDocuments,
+        }),
+      })
+
+      getQuestions()
+      onClose()
+      message.success('Question added.')
+    } catch (e) {
+      message.error('Failed to add question.' + e)
+    } finally {
+      form.resetFields()
+    }
+  }
+
+  return (
+    <Modal
+      title="Create a new question for your students!"
+      visible={visible}
+      onCancel={onClose}
+      footer={[
+        <Button key="cancel" type="ghost" onClick={onClose}>
+          Cancel
+        </Button>,
+        <Button key="submit" type="primary" onClick={addQuestion}>
+          Submit
+        </Button>,
+      ]}
+    >
+      <Form form={form}>
+        <Form.Item
+          label="Question"
+          name="questionText"
+          rules={[
+            {
+              required: true,
+              message: 'Please input a question!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Answer"
+          name="responseText"
+          rules={[
+            {
+              required: true,
+              message: 'Please input an answer!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Verified"
+          name="verified"
+          valuePropName="checked"
+          initialValue={true}
+        >
+          <Switch />
+        </Form.Item>
+        <Form.Item
+          label="Suggested"
+          name="suggested"
+          valuePropName="checked"
+          initialValue={false}
+        >
+          <Switch />
+        </Form.Item>
+        <Select
+          className="my-4"
+          placeholder="Select a document to add"
+          style={{ width: '100%' }}
+          onSelect={(selectedDocId) => {
+            const selectedDoc = existingDocuments.find(
+              (doc) => doc.docId === selectedDocId,
+            )
+            if (selectedDoc) {
+              setSelectedDocuments((prev) => {
+                const isAlreadySelected = prev.some(
+                  (doc) => doc.docId === selectedDocId,
+                )
+                if (!isAlreadySelected) {
+                  return [...prev, { ...selectedDoc, pageNumbers: [] }]
+                }
+                return prev
+              })
+            }
+          }}
+        >
+          {existingDocuments.map((doc) => (
+            <Select.Option key={doc.docId} value={doc.docId}>
+              {doc.docName}
+            </Select.Option>
+          ))}
+        </Select>
+
+        {selectedDocuments.map((doc, index) => (
+          <div key={doc.docId}>
+            <span className="font-bold">{doc.docName}</span>
+            <Input
+              type="text"
+              placeholder="Enter page numbers (comma separated)"
+              value={doc.pageNumbers}
+              onChange={(e) => {
+                const updatedPageNumbers = e.target.value
+                // Split by comma, trim whitespace, filter empty strings, convert to numbers
+                const pageNumbersArray = updatedPageNumbers
+                  .split(',')
+                  .map(Number)
+                setSelectedDocuments((prev) =>
+                  prev.map((d, idx) =>
+                    idx === index
+                      ? { ...d, pageNumbers: pageNumbersArray } // array of numbers
+                      : d,
+                  ),
+                )
+              }}
+            />
+          </div>
+        ))}
+      </Form>
+    </Modal>
+  )
+}
