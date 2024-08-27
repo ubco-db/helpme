669835c49e2e4fe7d3958a75d14f10ebd085955b
wskksw <66136750+wskksw@users.noreply.github.com>
2024-08-27 10:38:25 -0700

    get rid of localhost headers

--- packages/frontend/app/api/courseApi.ts
+++ packages/frontend/app/api/courseApi.ts
@@ -9,36 +9,30 @@ import { fetchAuthToken } from './cookieApi'
 export const courseApi = {
   getCourseFeatures: async (courseId: number) => {
     const authToken = await fetchAuthToken()
-    const response = await fetch(
-      `http://localhost:3000/api/v1/courses/${courseId}/features`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: authToken,
-          Cookie: authToken,
-        },
-        credentials: 'include',
+    const response = await fetch(`/api/v1/courses/${courseId}/features`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authToken,
+        Cookie: authToken,
       },
-    )
+      credentials: 'include',
+    })
 
     return response.json()
   },
 
   getCourse: async (courseId: number): Promise<GetCourseResponse> => {
     const authToken = await fetchAuthToken()
-    const response = await fetch(
-      `http://localhost:3000/api/v1/courses/${courseId}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: authToken,
-          Cookie: authToken,
-        },
-        credentials: 'include',
+    const response = await fetch(`/api/v1/courses/${courseId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authToken,
+        Cookie: authToken,
       },
-    )
+      credentials: 'include',
+    })
 
     return response.json()
   },
--- packages/frontend/app/api/organizationApi.ts
+++ packages/frontend/app/api/organizationApi.ts
@@ -7,32 +7,27 @@ import { fetchAuthToken } from './cookieApi'
  */
 export const organizationApi = {
   getOrganizations: async () => {
-    const response = await fetch(`http://localhost:3000/api/v1/organization`)
+    const response = await fetch(`/api/v1/organization`)
     return response.json()
   },
 
   getOrganization: async (organizationId: number) => {
     const authToken = await fetchAuthToken()
 
-    const response = await fetch(
-      `http://localhost:3000/api/v1/organization/${organizationId}`,
-      {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: authToken,
-          Cookie: authToken,
-        },
-        credentials: 'include',
+    const response = await fetch(`/api/v1/organization/${organizationId}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: authToken,
+        Cookie: authToken,
       },
-    )
+      credentials: 'include',
+    })
     return response.json()
   },
 
   getOrganizationStats: async (organizationId: number) => {
-    const response = await fetch(
-      `http://localhost:3000/api/v1/organization/${organizationId}/stats`,
-    )
+    const response = await fetch(`/api/v1/organization/${organizationId}/stats`)
     return response.json()
   },
 }
