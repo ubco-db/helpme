5efc8de52daabbde630146ffd66265e91eae9b97
wskksw <66136750+wskksw@users.noreply.github.com>
2024-09-04 14:05:04 -0700

    deleted old app folder

--- packages/app/.eslintrc.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  extends: ["plugin:react/recommended", "plugin:react-hooks/recommended"],
-  rules: { "react/react-in-jsx-scope": "off" },
-  globals: { React: "writable" },
-};
--- packages/app/hooks/useIsMobile.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    setIsMobile(window.innerWidth < 768);
-  }, []);
-  return isMobile;
-};
