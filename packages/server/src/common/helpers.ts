b45e1d779456e75189562192ab4204c2bf78230b
Adam Fipke <adamfipke@gmail.com>
2024-11-02 01:09:08 -0700

    hopefully fixed sentry on server again

--- packages/server/src/common/helpers.ts
+++ packages/server/src/common/helpers.ts
@@ -14,19 +14,3 @@ export function getCookie(req: Request, cookieName: string): string {
     }
   }
 }
-
-// This is code from common index.ts. It doesn't always work in there since it's not always initialized for some reason.
-// So I had to put this in the server as well, that way sentry can run.
-export const PROD_URL = 'https://coursehelp.ubc.ca';
-const domain = (): string | false =>
-  process.env.DOMAIN ||
-  (typeof window !== 'undefined' && window?.location?.origin);
-export const getEnv = (): 'production' | 'dev' => {
-  switch (domain()) {
-    case PROD_URL:
-      return 'production';
-    default:
-      return 'dev';
-  }
-};
-export const isProd = (): boolean => domain() === PROD_URL;
