bedc421f7799e08d6cb4a8b16cc9035674e6664f
Adam Fipke <adamfipke@gmail.com>
2024-11-15 23:41:47 -0800

    some cleanup. Added a check to delete old cron jobs that should not exist

--- packages/server/src/calendar/calendar.service.ts
+++ packages/server/src/calendar/calendar.service.ts
@@ -4,68 +4,28 @@ import {
   NotFoundException,
   OnModuleInit,
 } from '@nestjs/common';
-import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { SchedulerRegistry } from '@nestjs/schedule';
 import { CalendarStaffModel } from './calendar-staff.entity';
-import {
-  CalendarStaff,
-  CalendarStaffRedisService,
-} from './calendar-staff-redis.service';
-import { createQueryBuilder, EntityManager, In } from 'typeorm';
+import { EntityManager } from 'typeorm';
 import { UserModel } from 'profile/user.entity';
 import { CalendarModel } from './calendar.entity';
 import { AlertModel } from 'alerts/alerts.entity';
-import {
-  AlertType,
-  ClosedQuestionStatus,
-  ERROR_MESSAGES,
-  OpenQuestionStatus,
-} from '@koh/common';
+import { AlertType, ERROR_MESSAGES } from '@koh/common';
 import { QueueModel } from 'queue/queue.entity';
 import { EventModel, EventType } from 'profile/event-model.entity';
 import { CronJob } from 'cron';
 import * as Sentry from '@sentry/browser';
-import { QuestionModel } from 'question/question.entity';
 import { QuestionService } from 'question/question.service';
 
 @Injectable()
 export class CalendarService implements OnModuleInit {
   constructor(
-    // private readonly calStaffRedisService: CalendarStaffRedisService,
     private schedulerRegistry: SchedulerRegistry,
     private questionService: QuestionService,
   ) {}
 
   async onModuleInit() {
-    await this.initializeCache();
     await this.resetAutoCheckoutJobs();
-  }
-
-  async initializeCache() {
-    console.log('Initializing calendar staff into redis cache');
-    const calendarStaff = await createQueryBuilder(CalendarStaffModel)
-      .select([
-        'CalendarStaffModel.userId AS userId',
-        'CalendarStaffModel.calendarId AS calendarId',
-        'staff.firstName || staff.lastName AS username',
-        'calendar.startDate',
-        'calendar.endDate',
-        'calendar.start AS startTime',
-        'calendar.end AS endTime',
-        'calendar.daysOfWeek',
-        'calendar.course AS courseId',
-      ])
-      .leftJoin(UserModel, 'staff', 'staff.id = CalendarStaffModel.userId')
-      .leftJoin(
-        CalendarModel,
-        'calendar',
-        'calendar.id = CalendarStaffModel.calendarId',
-      )
-      .getRawMany<CalendarStaff>();
-    // await this.calStaffRedisService.setAllCalendarStaff(
-    //   'calendar-staff',
-    //   calendarStaff,
-    // );
-    return calendarStaff;
   }
 
   /** save the calendar staff model (for many to many relationship) */
@@ -74,12 +34,8 @@ export class CalendarService implements OnModuleInit {
     calendar: CalendarModel,
     transactionalEntityManager: EntityManager,
   ) {
-    // make sure the user and the calendar event exist (while also retrieving some data to put inside redis)
-    const user = await transactionalEntityManager
-      .createQueryBuilder(UserModel, 'staff')
-      .select('staff.firstName || staff.lastName AS name')
-      .where('staff.id = :userId', { userId })
-      .getRawOne<{ name: string }>();
+    // make sure the user exists
+    const user = await transactionalEntityManager.findOne(UserModel, userId);
     if (!user) {
       throw new NotFoundException('User not found');
     }
@@ -87,22 +43,6 @@ export class CalendarService implements OnModuleInit {
       userId,
       calendarId: calendar.id,
     });
-    // // get the user and calendar data to store in redis
-    // const calendarStaff: CalendarStaff = {
-    //   userId,
-    //   calendarId: calendar.id,
-    //   courseId: calendar.course.id,
-    //   username: user.name,
-    //   startDate: calendar.startDate,
-    //   endDate: calendar.endDate,
-    //   startTime: calendar.start,
-    //   endTime: calendar.end,
-    //   daysOfWeek: calendar.daysOfWeek,
-    // };
-    // // await this.calStaffRedisService.setCalendarStaff(
-    // //   'calendar-staff',
-    // //   calendarStaff,
-    // // );
   }
 
   async deleteAllCalendarStaffForCalendar(
@@ -112,102 +52,6 @@ export class CalendarService implements OnModuleInit {
     await transactionalEntityManager.delete(CalendarStaffModel, { calendarId });
   }
 
-  // TODO: updateCalendarStaff
-  // async updateCalendarStaff(
-
-  /** delete the calendar staff model (for many to many relationship) */
-  // async deleteCalendarStaff(
-  //   userId: number,
-  //   calendarId: number,
-  //   calendarAlreadyDeleted = false,
-  // ) {
-  //   if (!calendarAlreadyDeleted) {
-  //     await CalendarStaffModel.delete({ userId, calendarId });
-  //   }
-  //   // await this.calStaffRedisService.deleteCalendarStaff(
-  //   //   'calendar-staff',
-  //   //   userId,
-  //   //   calendarId,
-  //   // );
-  // }
-
-  // So every time a calendar-staff is created, it gets added to redis
-  // Every 10 minutes, cycle through all calendar-staff and send an alert that they are going to be auto-checked out (or just check them out).
-  // @Cron(CronExpression.EVERY_10_MINUTES)
-  // async autoCheckOutStaff() {
-  //   // Check the length of the redis keys. If 0, re-initialize the cache
-  //   let redisCount =
-  //     await this.calStaffRedisService.getKeyCount('calendar-staff');
-  //   if (redisCount === 0) {
-  //     await this.initializeCache();
-  //   }
-  //   // If still 0 (as in no events have any staff assigned to them), just return
-  //   redisCount = await this.calStaffRedisService.getKeyCount('calendar-staff');
-  //   if (redisCount === 0) {
-  //     return;
-  //   }
-  //   const allCalendarStaff =
-  //     await this.calStaffRedisService.getCalendarStaff('calendar-staff');
-  //   // Perform the auto-checkout logic using the data from Redis
-  //   console.log('calendarStaff' + JSON.stringify(allCalendarStaff));
-  //   Object.values(allCalendarStaff).forEach(async (calendarStaff) => {
-  //     console.log('staff' + JSON.stringify(calendarStaff));
-
-  //     // apparently typeORM doesn't really provide a nice way to *only* query the @JoinTable() stafflist
-  //     // thus, i will use a raw query
-  //     const query = `
-  //       SELECT "queueModelId" AS queueId
-  //       FROM queue_model_staff_list_user_model
-  //       WHERE "userModelId" = $1
-  //       `;
-  //     const myCheckedInQueues: { queueId: number }[] = await QueueModel.query(
-  //       query,
-  //       [calendarStaff.userId],
-  //     );
-
-  //     if (myCheckedInQueues.length === 0) {
-  //       return;
-  //     }
-
-  //     const anotherAlert = await AlertModel.findOne({
-  //       where: {
-  //         alertType: AlertType.EVENT_ENDED_CHECKOUT_STAFF,
-  //         userId: calendarStaff.userId,
-  //         resolved: null,
-  //       },
-  //     });
-
-  //     // If the same user already has an alert for this then don't create a new one and instead checkout the TA
-  //     if (anotherAlert) {
-  //       const queuesWithCheckedInStaff: QueueModel[] =
-  //         await QueueModel.getRepository().find({ relations: ['staffList'] });
-  //       queuesWithCheckedInStaff.forEach(async (queue) => {
-  //         queue.staffList.forEach(async (ta) => {
-  //           await EventModel.create({
-  //             time: new Date(),
-  //             eventType: EventType.TA_CHECKED_OUT_EVENT_END,
-  //             userId: ta.id,
-  //             courseId: queue.courseId,
-  //             queueId: queue.id,
-  //           }).save();
-  //         });
-  //         queue.staffList = [];
-  //       });
-  //       await QueueModel.save(queuesWithCheckedInStaff);
-  //     } else {
-  //       const alert = await AlertModel.create({
-  //         alertType: AlertType.EVENT_ENDED_CHECKOUT_STAFF,
-  //         sent: new Date(),
-  //         userId: calendarStaff.userId,
-  //         courseId: calendarStaff.courseId,
-  //         payload: {},
-  //       }).save();
-  //     }
-  //   });
-  // }
-
-  // NOTE: scrapping everything above and isntead going to use dynamic cron jobs with nest.js
-  // Not using redis
   /** Creates a new Cron job with name based on the userId and calendarId */
   async createAutoCheckoutCronJob(
     userId: number,
@@ -250,7 +94,7 @@ export class CalendarService implements OnModuleInit {
       throw new BadRequestException(ERROR_MESSAGES.calendarEvent.invalidEvent);
     }
 
-    const job = new CronJob(jobInterval, () => {
+    const job = new CronJob(jobInterval, async () => {
       // if startDate is in the future, then don't do anything
       if (startDate && startDate > new Date()) {
         return;
@@ -261,6 +105,15 @@ export class CalendarService implements OnModuleInit {
         this.deleteAutoCheckoutCronJob(userId, calendarId);
         return;
       }
+      // make sure the userId and the calendarId exist. If not, goodbye
+      await UserModel.findOneOrFail(userId).catch(() => {
+        this.deleteAutoCheckoutCronJob(userId, calendarId);
+        return;
+      });
+      await CalendarModel.findOneOrFail(calendarId).catch(() => {
+        this.deleteAutoCheckoutCronJob(userId, calendarId);
+        return;
+      });
       // otherwise, initialize the auto-checkout loop
       this.initializeAutoCheckout(userId, calendarId, courseId);
     });
@@ -523,10 +376,4 @@ export class CalendarService implements OnModuleInit {
     await this.retroactivelyCreateAutoCheckoutJobs();
     console.log('Retroactively created auto-checkout jobs');
   }
-
-  // also add a service that will clear all old cron jobs with a particular calendar Id
-  // as well as another that will clear all cron jobs with a particular userId (for when they get deleted)
-  // somehow maybe add a database trigger for this?
-
-  // TODO: on launch, restart all cron jobs (maybe)
 }
