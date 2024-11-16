a6641cf728098c33e017fc7c241aed8fcf223240
Adam Fipke <adamfipke@gmail.com>
2024-11-15 23:48:58 -0800

    whoops forgot this

--- packages/server/src/calendar/calendar.module.ts
+++ packages/server/src/calendar/calendar.module.ts
@@ -3,18 +3,12 @@ import { QueueModule } from '../queue/queue.module';
 import { LoginModule } from '../login/login.module';
 import { CalendarService } from './calendar.service';
 import { CalendarController } from './calendar.controller';
-import { CalendarStaffRedisService } from './calendar-staff-redis.service';
 import { QuestionService } from '../question/question.service';
 import { NotificationService } from 'notification/notification.service';
 
 @Module({
   controllers: [CalendarController],
   imports: [QueueModule, LoginModule],
-  providers: [
-    CalendarService,
-    CalendarStaffRedisService,
-    QuestionService,
-    NotificationService,
-  ],
+  providers: [CalendarService, QuestionService, NotificationService],
 })
 export class CalendarModule {}
