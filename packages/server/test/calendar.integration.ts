9abca0978639f7b0e24d275d04ee023b69850e9f
Adam Fipke <adamfipke@gmail.com>
2024-11-23 15:53:26 -0800

    deleted nice cron job integration tests. May bring back in future if I can figure out how to usefaketimers with cron jobs that do queries

--- packages/server/test/calendar.integration.ts
+++ packages/server/test/calendar.integration.ts
@@ -5,20 +5,14 @@ import {
   calendarFactory,
   OrganizationFactory,
   OrganizationUserFactory,
-  QueueFactory,
-  QuestionFactory,
-  AsyncQuestionFactory,
 } from './util/factories';
 import { setupIntegrationTest } from './util/testUtils';
 import { CalendarModel } from '../src/calendar/calendar.entity';
 import { CalendarModule } from '../src/calendar/calendar.module';
 import {
-  AlertType,
   calendarEventLocationType,
-  ClosedQuestionStatus,
   CronJob,
   ERROR_MESSAGES,
-  OpenQuestionStatus,
   OrganizationRole,
   Role,
 } from '@koh/common';
@@ -27,9 +21,6 @@ import { OrganizationModule } from '../src/organization/organization.module';
 import { CourseModel } from 'course/course.entity';
 import { UserModel } from 'profile/user.entity';
 import { OrganizationModel } from 'organization/organization.entity';
-import { AlertModel } from 'alerts/alerts.entity';
-import { QueueModel } from 'queue/queue.entity';
-import { QuestionModel } from 'question/question.entity';
 
 describe('Calendar Integration', () => {
   const supertest = setupIntegrationTest(CalendarModule, undefined, [
@@ -613,176 +604,5 @@ describe('Calendar Integration', () => {
       );
       expect(autoCheckoutJobsAfter).toHaveLength(0);
     });
-  });
-
-  describe('Auto-checkout cron jobs', () => {
-    let cronJobSpy: jest.SpyInstance;
-    beforeEach(async () => {
-      // jest's fake timers mess with dates when creating tables
-      // and typeorm creates tables sometimes in the middle of tests
-      await AsyncQuestionFactory.create();
-      const tempcalendar = await calendarFactory.create();
-      await CalendarStaffModel.create({
-        // for some reason I have to create this otherwise I get "calendar_staff_model does not exit" error
-        user: ta1,
-        calendar: tempcalendar,
-        userId: ta1.id,
-        calendarId: tempcalendar.id,
-      }).save();
-      jest.useFakeTimers({
-        doNotFake: [
-          // "nextTick",
-          // "setImmediate",
-          // "clearImmediate",
-          // "setInterval",
-          // "clearInterval",
-          'setTimeout', // this one seems to be the only one necessary to get typeorm to not fail
-          // "clearTimeout",
-        ],
-        now: todayAt7,
-        advanceTimers: true,
-      });
-      cronJobSpy = jest.spyOn(global, 'setTimeout');
-    });
-
-    afterEach(() => {
-      jest.useRealTimers();
-      cronJobSpy.mockRestore();
-    });
-    it('will automatically checkout a TA (and stop helping any questions) 10mins after the event ends if the alert is not resolved', async () => {
-      // create a queue, and put two staff in it
-      let queue = await QueueFactory.create({ course, staffList: [ta1, prof] });
-      // create some questions, one queued, one being helped by ta1
-      let q1 = await QuestionFactory.create({
-        queue,
-        status: OpenQuestionStatus.Queued,
-      });
-      let q2 = await QuestionFactory.create({
-        queue,
-        status: OpenQuestionStatus.Helping,
-        taHelped: ta1,
-      });
-      // create an event using the endpoint, that way the cron jobs are made
-      const eventData = {
-        title: 'Test Event',
-        start: todayAt7,
-        end: todayAt8,
-        daysOfWeek: [0, 1, 2, 3, 4, 5, 6],
-        startDate: todayAt7,
-        endDate: oneMonthFromNow,
-        locationType: calendarEventLocationType.online,
-        locationOnline: 'https://zoom.us/test',
-        allDay: false,
-        staffIds: [ta1.id, ta2.id, prof.id],
-      };
-      const eventRes = await supertest({ userId: prof.id })
-        .post(`/calendar/${course.id}`)
-        .send(eventData)
-        .expect(201);
-      const _event: CalendarModel = eventRes.body;
-
-      // go through the jobs, filter and find all the auto-checkout jobs
-      let jobsRes = await supertest({ userId: prof.id }).get(
-        `/organization/${org.id}/cronjobs`,
-      );
-      let jobs = jobsRes.body;
-      let autoCheckoutJobs: CronJob[] = jobs.filter((job: CronJob) =>
-        job.id.includes('auto-checkout'),
-      );
-      expect(autoCheckoutJobs).toHaveLength(3);
-      for (const job of autoCheckoutJobs) {
-        expect(job.cronTime).toMatch(/^[0-2] 8 \* \* [0-6](,[0-6])*$/);
-        expect(job.running).toBe(true);
-        expect(job.runOnce).toBe(false);
-        expect(new Date(job.nextDates[0])).toEqual(todayAt8);
-      }
-      // fast forward 1 hour and 5 minutes
-      console.log(new Date());
-      // jest.advanceTimersByTime(24* 65 * 60 * 1000);
-      // await jest.advanceTimersByTimeAsync(24* 65 * 60 * 1000);
-      jest.advanceTimersByTime(24 * 65 * 60 * 1000);
-      // await jest.runAllTimersAsync();
-      jest.runAllTimers();
-      jest.runAllTicks();
-      // jest.runAllImmediates()
-
-      console.log(new Date());
-      expect(cronJobSpy).toHaveBeenCalled();
-      // make sure the alerts were created
-      const alerts = await AlertModel.find();
-      console.log(alerts);
-      expect(alerts).toHaveLength(2);
-      for (const alert of alerts) {
-        expect(alert.resolved).toBeNull();
-        expect(alert.alertType).toBe(AlertType.EVENT_ENDED_CHECKOUT_STAFF);
-        // make sure the user id is one of the staff
-        expect([ta1.id, prof.id]).toContain(alert.userId);
-      }
-      // make sure there are two auto-checkout-loop cron jobs
-      jobsRes = await supertest({ userId: prof.id }).get(
-        `/organization/${org.id}/cronjobs`,
-      );
-      jobs = jobsRes.body;
-      console.log(jobs);
-      let autoCheckoutLoopJobs: CronJob[] = jobs.filter((job: CronJob) =>
-        job.id.includes('auto-checkout-loop'),
-      );
-      // expect(autoCheckoutLoopJobs).toHaveLength(2);
-      const tenMinutesFromNow = new Date(todayAt8.getTime() + 10 * 60 * 1000);
-      // for (const job of autoCheckoutLoopJobs) {
-      //   expect(job.cronTime).toEqual(tenMinutesFromNow);
-      //   expect(job.running).toBe(true);
-      //   expect(job.runOnce).toBe(true);
-      // }
-      // make sure the auto-checkout jobs are still running
-      autoCheckoutJobs = jobs.filter(
-        (job: CronJob) =>
-          job.id.includes('auto-checkout') && !job.id.includes('loop'),
-      );
-      expect(autoCheckoutJobs).toHaveLength(3);
-      const oneDayFromNow = new Date(todayAt8.getTime() + 24 * 60 * 60 * 1000);
-      for (const job of autoCheckoutJobs) {
-        expect(job.running).toBe(true);
-        expect(job.runOnce).toBe(false);
-        // make sure the next run date is tomorrow
-        expect(new Date(job.nextDates[0])).toEqual(oneDayFromNow);
-      }
-      // check to make sure the staff are still in the queue and question is unchanged
-      queue = await QueueModel.findOne(queue.id, { relations: ['staffList'] });
-      expect(queue.staffList.map((s) => s.id)).toEqual([ta1.id, prof.id]);
-      q1 = await QuestionModel.findOne(q1.id);
-      expect(q1.status).toBe(OpenQuestionStatus.Queued);
-      q2 = await QuestionModel.findOne(q2.id);
-      expect(q2.status).toBe(OpenQuestionStatus.Helping);
-      // fast forward 10 minutes
-      jest.advanceTimersByTime(10 * 60 * 1000);
-      // check to make sure the staff are no longer in the queue
-      queue = await QueueModel.findOne(queue.id, { relations: ['staffList'] });
-      expect(queue.staffList).toHaveLength(0);
-      // check to make sure the helping question has be resolved
-      q2 = await QuestionModel.findOne(q2.id);
-      expect(q2.status).toBe(ClosedQuestionStatus.Resolved);
-      // make sure queued question is still queued
-      q1 = await QuestionModel.findOne(q1.id);
-      expect(q1.status).toBe(OpenQuestionStatus.Queued);
-      // check to make sure the alerts are resolved
-      const resolvedAlerts = await AlertModel.find();
-      for (const alert of resolvedAlerts) {
-        expect(alert.resolved).not.toBeNull();
-      }
-      // make sure the auto-checkout-loop cron jobs are no longer running (deleted)
-      jobsRes = await supertest({ userId: prof.id }).get(
-        `/organization/${org.id}/cronjobs`,
-      );
-      jobs = jobsRes.body;
-      autoCheckoutLoopJobs = jobs.filter((job: CronJob) =>
-        job.id.includes('auto-checkout-loop'),
-      );
-      expect(autoCheckoutLoopJobs).toHaveLength(0);
-    });
-    // it('will not create a new alert and cron job if the TA is already checked out by the time the event ends')
-    // it('if the alert is resolved and the TA is checked out, no alert or cron job will be created')
-    // it('if the alert is resolved and the TA is still checked in, it will create a new cron job 10 minutes from the resolve date which will create a new alert. This can happen multiple times (just test 3 times for this test)')
-    // it('will automatically checkout the TA multiple times for recurring events')
   });
 });
