bf8fe0114a65edd46a5c215f033d5353256012f7
Adam Fipke <adamfipke@gmail.com>
2024-12-17 17:26:03 -0800

    remove footer fragment

--- packages/frontend/app/(dashboard)/components/FooterBar.tsx
+++ packages/frontend/app/(dashboard)/components/FooterBar.tsx
@@ -6,37 +6,35 @@ const FooterBar: React.FC = () => {
 
   return (
     // Hide footer on mobile since screen space is more valuable
-    <>
-      <footer
-        className="mt-auto hidden w-full justify-between bg-[#ebebeb] px-6 py-1.5 text-xs md:flex"
-        aria-hidden="true"
-      >
-        <div>
-          Version 2.0.0{' '}
-          <a
-            className="ml-1 cursor-pointer text-blue-600 underline"
-            onClick={() => {
-              setIsChangelogOpen(true)
-            }}
-          >
-            What&apos;s new?
-          </a>
-          <ChangeLogModal
-            isOpen={isChangelogOpen}
-            setIsOpen={setIsChangelogOpen}
-          />
-        </div>
-        <div>
-          Found a bug? Have a suggestion? We welcome your feedback:
-          <a
-            href="mailto:adam.fipke@ubc.ca"
-            className="ml-1 text-blue-600 underline"
-          >
-            adam.fipke@ubc.ca
-          </a>
-        </div>
-      </footer>
-    </>
+    <footer
+      className="mt-auto hidden w-full justify-between bg-[#ebebeb] px-6 py-1.5 text-xs md:flex"
+      aria-hidden="true"
+    >
+      <div>
+        Version 2.0.0{' '}
+        <a
+          className="ml-1 cursor-pointer text-blue-600 underline"
+          onClick={() => {
+            setIsChangelogOpen(true)
+          }}
+        >
+          What&apos;s new?
+        </a>
+        <ChangeLogModal
+          isOpen={isChangelogOpen}
+          setIsOpen={setIsChangelogOpen}
+        />
+      </div>
+      <div>
+        Found a bug? Have a suggestion? We welcome your feedback:
+        <a
+          href="mailto:adam.fipke@ubc.ca"
+          className="ml-1 text-blue-600 underline"
+        >
+          adam.fipke@ubc.ca
+        </a>
+      </div>
+    </footer>
   )
 }
 
