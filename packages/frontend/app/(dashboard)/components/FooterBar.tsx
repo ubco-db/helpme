9862ca8cd70b97ff07001b22995c0853eb265369
Adam Fipke <adamfipke@gmail.com>
2024-12-17 18:58:05 -0800

    Made it so it shows '(new changes)' on the footer instead of opening the changelog modal immediately for users that have not seen new changes

--- packages/frontend/app/(dashboard)/components/FooterBar.tsx
+++ packages/frontend/app/(dashboard)/components/FooterBar.tsx
@@ -1,8 +1,33 @@
+import { API } from '@/app/api'
 import ChangeLogModal from '@/app/components/ChangeLogModal'
+import { useUserInfo } from '@/app/contexts/userContext'
+import { getErrorMessage } from '@/app/utils/generalUtils'
 import { useState } from 'react'
 
 const FooterBar: React.FC = () => {
   const [isChangelogOpen, setIsChangelogOpen] = useState(false)
+
+  const { userInfo, setUserInfo } = useUserInfo()
+
+  const IReadTheChangelog = () => {
+    setUserInfo({ ...userInfo, readChangeLog: true })
+
+    const readChangelog = async () => {
+      if (!userInfo?.readChangeLog) {
+        try {
+          await API.profile.readChangelog()
+        } catch (error) {
+          const errorMessage = getErrorMessage(error)
+          console.error(
+            'Failed to tell server that changelog was read:',
+            errorMessage,
+          )
+        }
+      }
+    }
+
+    readChangelog()
+  }
 
   return (
     // Hide footer on mobile since screen space is more valuable
@@ -11,14 +36,17 @@ const FooterBar: React.FC = () => {
       aria-hidden="true"
     >
       <div>
-        Version 2.0.0{' '}
         <a
-          className="ml-1 cursor-pointer text-blue-600 underline"
+          className="ml-1 cursor-pointer "
           onClick={() => {
             setIsChangelogOpen(true)
+            IReadTheChangelog()
           }}
         >
-          What&apos;s new?
+          Version 1.1.1{' '}
+          {userInfo.readChangeLog === false && (
+            <span className="text-green-500">(New Changes!)</span>
+          )}
         </a>
         <ChangeLogModal
           isOpen={isChangelogOpen}
--- packages/frontend/app/components/ChangeLogModal.tsx
+++ packages/frontend/app/components/ChangeLogModal.tsx
@@ -1,8 +1,6 @@
 import { Modal } from 'antd'
 import { useEffect, useState } from 'react'
-import { useUserInfo } from '../contexts/userContext'
 import MarkdownCustom from './Markdown'
-import { API } from '../api'
 
 interface ChangeLogModalProps {
   isOpen: boolean
@@ -13,14 +11,7 @@ const ChangeLogModal: React.FC<ChangeLogModalProps> = ({
   isOpen,
   setIsOpen,
 }) => {
-  const { userInfo, setUserInfo } = useUserInfo()
   const [currentChangeLog, setCurrentChangeLog] = useState<string>('')
-
-  useEffect(() => {
-    if (!userInfo?.readChangeLog) {
-      setIsOpen(true)
-    }
-  }, [userInfo, isOpen, setIsOpen])
 
   useEffect(() => {
     const fetchChangelog = async () => {
@@ -36,40 +27,23 @@ const ChangeLogModal: React.FC<ChangeLogModalProps> = ({
     fetchChangelog()
   }, [setCurrentChangeLog])
 
-  const onClose = () => {
-    setIsOpen(false)
-    setUserInfo({ ...userInfo, readChangeLog: true })
-
-    const readChangelog = async () => {
-      if (!userInfo?.readChangeLog) {
-        try {
-          await API.profile.readChangelog()
-        } catch (error) {
-          console.error('Failed to update user info:', error)
-        }
-      }
-    }
-
-    readChangelog()
-  }
-
   return (
-    <>
-      <Modal
-        title={<span>What&apos;s New?</span>}
-        open={isOpen}
-        destroyOnClose
-        closable
-        footer={null}
-        onCancel={onClose}
-        width="auto"
-        className="box-border flex flex-col items-center justify-center overflow-y-auto"
-      >
-        <div className="box-border h-[75vh] w-full overflow-y-auto px-4">
-          <MarkdownCustom>{currentChangeLog}</MarkdownCustom>
-        </div>
-      </Modal>
-    </>
+    <Modal
+      title={<h1>What&apos;s New?</h1>}
+      open={isOpen}
+      closable
+      footer={null}
+      onCancel={() => setIsOpen(false)}
+      width="auto"
+      classNames={{
+        header: 'flex items-center justify-center',
+      }}
+      className="box-border flex flex-col items-center justify-center overflow-y-auto"
+    >
+      <div className="childrenMarkdownFormatted box-border h-[75vh] w-full overflow-y-auto px-4">
+        <MarkdownCustom>{currentChangeLog}</MarkdownCustom>
+      </div>
+    </Modal>
   )
 }
 export default ChangeLogModal
