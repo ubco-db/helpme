288b9f277083fd5418fb8c8161a825faf9e813c8
bhunt02 <bridgette.hunt02@outlook.com>
2025-01-29 09:05:36 -0800

    slight change

--- packages/server/migration/1738106924799-remove-organization-user-model.ts
+++ packages/server/migration/1738106924799-remove-organization-user-model.ts
@@ -1,5 +1,4 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import { UserModel } from '../src/profile/user.entity';
 
 export class removeOrganizationUserModel1738106924799
   implements MigrationInterface
@@ -7,64 +6,6 @@ export class removeOrganizationUserModel1738106924799
   name = 'removeOrganizationUserModel1738106924799';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    /*
-    Irreversible operation to collapse accounts with collisions
-    (e.g., accounts with same normalized email address and organization ID)
-    into one account
-     */
-    const emailsWithCollisions: {
-      normalizedEmail: string;
-      organizationId: number;
-    }[] = await queryRunner.query(`
-      SELECT UPPER("email") AS "normalizedEmail", "organizationId"
-      FROM "user_model" JOIN "organization_user_model" ON "user_model"."id" = "organization_user_model"."userId"
-      GROUP BY UPPER("email"), "organizationId"
-      HAVING COUNT(UPPER("email")) > 1;
-    `);
-    for (const email of emailsWithCollisions) {
-      const matchingUsers = await queryRunner.manager
-        .createQueryBuilder(UserModel, 'um')
-        .select()
-        .where('UPPER("um"."email") = :email', { email })
-        .orderBy('id', 'ASC')
-        .getMany();
-      if (matchingUsers.length > 0) {
-        const primeUser = matchingUsers[0];
-        const otherUsers = matchingUsers.slice(1);
-        for (const otherUser of otherUsers) {
-          // This might be overkill, but I don't know when this will finally end up being run
-          // No idea how many collisions there will be by then
-          // Update each table with a FK to user's ID column to grant the 'prime' user ownership
-          await queryRunner.manager.query(`
-            UPDATE "alert_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-            UPDATE "async_question_model" SET "creatorId" = ${primeUser.id} WHERE "creatorId" = ${otherUser.id};
-            UPDATE "async_question_model" SET "taHelpedId" = ${primeUser.id} WHERE "taHelpedId" = ${otherUser.id};
-            UPDATE "async_question_votes_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id}
-            UPDATE "calendar_staff_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-            UPDATE "chat_token_model" SET "user" = ${primeUser.id} WHERE "user" = ${otherUser.id};
-            UPDATE "chatbot_interactions_model" SET "user" = ${primeUser.id} WHERE "user" = ${otherUser.id};
-            UPDATE "desktop_notif_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-            UPDATE "event_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-            UPDATE "question_group_model" SET "creatorId" = ${primeUser.id} WHERE "creatorId" = ${otherUser.id};
-            UPDATE "question_model" SET "creatorId" = ${primeUser.id} WHERE "creatorId" = ${otherUser.id};
-            UPDATE "question_model" SET "taHelpedId" = ${primeUser.id} WHERE "taHelpedId" = ${otherUser.id};
-            UPDATE "queue_model_staff_list_user_model" SET "userModelId" = ${primeUser.id} WHERE "userModelId" = ${otherUser.id};
-            UPDATE "student_task_progress_model" SET "uid" = ${primeUser.id} WHERE "uid" = ${otherUser.id};
-            UPDATE "user_course_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-            UPDATE "user_subscriptions" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-            UPDATE "user_token_model" SET "userId" = ${primeUser.id} WHERE "userId" = ${otherUser.id};
-
-            DELETE FROM user_model WHERE "id" = ${otherUser.id}
-          `);
-        }
-      }
-    }
-
-    const usersWithCollisions = queryRunner.manager
-      .createQueryBuilder(UserModel, 'UserModel')
-      .select()
-      .where('UPPER("UserModel"."email")');
-
     /*
     Query(ies) to transfer columns from and drop the OrganizationUserModel, and create the normalizedEmail column,
      */
