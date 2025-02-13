e99ed982993b68d7240421cf7f0f3067df02a438
Adam Fipke <adamfipke@gmail.com>
2025-02-12 17:53:46 -0800

    made CourseRosterTable look a little nicer on mobile

--- packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/components/CourseRosterTable.tsx
+++ packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/components/CourseRosterTable.tsx
@@ -19,6 +19,7 @@ import {
   Modal,
   Pagination,
   Popover,
+  Row,
   Spin,
   Tooltip,
 } from 'antd'
@@ -189,42 +190,178 @@ const RosterItem: React.FC<{
   return (
     <List.Item
       key={item.id}
-      className={cn('flex items-center justify-between', className ?? '')}
-    >
-      <List.Item.Meta
-        avatar={
-          <UserAvatar photoURL={item.photoURL} username={item.name ?? ''} />
-        }
-        title={<span className="mr-2">{item.name}</span>}
-        className="flex flex-grow items-center"
-      />
-      {isSensitiveInfoHidden ? (
-        <span className="flex-grow">
-          {item.email
-            ?.substring(0, item.email?.indexOf('@'))
-            .replace(/./g, '*')}
-          {item.email?.substring(item.email?.indexOf('@'))}
-        </span>
-      ) : (
-        <span className="flex-grow">{item.email}</span>
+      className={cn(
+        'flex flex-col items-center justify-between px-1 py-2 md:flex-row md:px-4 md:py-4',
+        className ?? '',
       )}
+    >
+      <Row className="flex w-full items-center justify-between">
+        <List.Item.Meta
+          avatar={
+            <UserAvatar photoURL={item.photoURL} username={item.name ?? ''} />
+          }
+          title={<span className="mr-0 md:mr-2">{item.name}</span>}
+          className="flex flex-grow items-center"
+        />
+        {isSensitiveInfoHidden ? (
+          <div className="text-gray-600">
+            {item.email
+              ?.substring(0, item.email?.indexOf('@'))
+              .replace(/./g, '*')}
+            {item.email?.substring(item.email?.indexOf('@'))}
+          </div>
+        ) : (
+          <div className="text-gray-600">{item.email}</div>
+        )}
+      </Row>
 
-      <Dropdown
-        overlay={
-          <Menu
-            onClick={(e) => {
-              const confirmRoleChange = () => {
-                handleRoleChange(item.id, e.key as Role, item.name ?? '')
+      <Row className="flex w-full items-center justify-around md:justify-end">
+        <Dropdown
+          overlay={
+            <Menu
+              onClick={(e) => {
+                const confirmRoleChange = () => {
+                  handleRoleChange(item.id, e.key as Role, item.name ?? '')
+                }
+
+                Modal.confirm({
+                  title: <div className="font-bold">Warning</div>,
+                  content: (
+                    <div>
+                      You are about to change role of{' '}
+                      <span className="font-bold">{item.name}</span> to{' '}
+                      <span className="font-bold">{e.key.toUpperCase()}</span>
+                      .
+                      <br />
+                      <br />
+                      Are you sure you want to proceed?
+                    </div>
+                  ),
+                  okText: 'Yes',
+                  okType: 'danger',
+                  cancelText: 'No',
+                  onOk() {
+                    confirmRoleChange()
+                  },
+                })
+              }}
+            >
+              {role !== Role.PROFESSOR ? (
+                <Menu.Item key={Role.PROFESSOR}>Professor</Menu.Item>
+              ) : null}
+              {role !== Role.TA ? (
+                <Menu.Item key={Role.TA}>Teaching Assistant</Menu.Item>
+              ) : null}
+              {role !== Role.STUDENT ? (
+                <Menu.Item key={Role.STUDENT}>Student</Menu.Item>
+              ) : null}
+            </Menu>
+          }
+          className="flex-grow-0"
+        >
+          <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
+            Change Role <DownOutlined />
+          </a>
+        </Dropdown>
+
+        {(role === Role.TA || role === Role.PROFESSOR) && (
+          // NOTE: if you modify this popover, you may also want to make changes to the popover in StaffList
+          <Popover
+            trigger="click"
+            overlayClassName="min-w-80"
+            content={
+              <div className="flex flex-col gap-y-2">
+                <TextArea
+                  placeholder="Can answer questions about MATH 101, PHYS 102..."
+                  autoSize={{ minRows: 4, maxRows: 7 }}
+                  value={tempTaNotes}
+                  onChange={(e) => {
+                    setCanSave(e.target.value !== item.TANotes)
+                    setTempTaNotes(e.target.value)
+                  }}
+                />
+                <div className="flex items-center justify-start">
+                  <Button
+                    disabled={!canSave}
+                    loading={saveLoading}
+                    onClick={async () => {
+                      setSaveLoading(true)
+                      await API.course
+                        .updateTANotes(courseId, item.id, tempTaNotes ?? '')
+                        .then(() => {
+                          setSaveSuccessful(true)
+                          setCanSave(false)
+                          item.TANotes = tempTaNotes
+                          // saved goes away after 1s
+                          setTimeout(() => {
+                            setSaveSuccessful(false)
+                          }, 1000)
+                        })
+                        .catch((e) => {
+                          const errorMessage = getErrorMessage(e)
+                          message.error(errorMessage)
+                        })
+                        .finally(() => {
+                          setSaveLoading(false)
+                        })
+                    }}
+                  >
+                    Save
+                  </Button>
+                  <div>
+                    {
+                      <span
+                        className={`ml-2 text-green-500 transition-opacity duration-300 ${
+                          saveSuccessful ? 'opacity-100' : 'opacity-0'
+                        }`}
+                      >
+                        Saved!
+                      </span>
+                    }
+                  </div>
+                </div>
+              </div>
+            }
+            title={
+              <div className="flex items-center">
+                <div>{item.name} - TA Notes</div>
+                <div>
+                  <Tooltip title="Here you can set notes on your TAs (e.g. the types of questions a TA can answer). Other users can then hover the TA to see these notes. TAs are able to modify their own notes.">
+                    <span className="ml-2 text-gray-500">
+                      <QuestionCircleOutlined />
+                    </span>
+                  </Tooltip>
+                </div>
+              </div>
+            }
+          >
+            <Button
+              icon={
+                item.TANotes ? (
+                  <NotebookText className="p-[0.075rem] text-gray-700 transition-colors duration-200 ease-out hover:text-[#5ba1d4]" />
+                ) : (
+                  <Notebook className="p-[0.075rem] transition-colors duration-200 ease-out hover:text-[#5ba1d4]" />
+                )
               }
+              className="mx-2"
+            />
+          </Popover>
+        )}
 
+        {userInfo.id !== item.id && (
+          <Button
+            icon={<UserDeleteOutlined />}
+            danger
+            className={role === Role.STUDENT ? 'ml-2' : ''}
+            onClick={() => {
               Modal.confirm({
-                title: <div className="font-bold">Warning</div>,
+                title: <div className="font-bold text-red-600">Warning</div>,
                 content: (
                   <div>
-                    You are about to change role of{' '}
-                    <span className="font-bold">{item.name}</span> to{' '}
-                    <span className="font-bold">{e.key.toUpperCase()}</span>
-                    .
+                    You are about to{' '}
+                    <span className="text-red-600">remove</span>{' '}
+                    <span className="font-bold">{item.name}</span> from the
+                    course.
                     <br />
                     <br />
                     Are you sure you want to proceed?
@@ -233,159 +370,31 @@ const RosterItem: React.FC<{
                 okText: 'Yes',
                 okType: 'danger',
                 cancelText: 'No',
+
                 onOk() {
-                  confirmRoleChange()
+                  API.organizations
+                    .dropUserCourses(
+                      userInfo.organization?.orgId ?? -1,
+                      item.id,
+                      [courseId],
+                    )
+                    .then(() => {
+                      message.success(
+                        `${item.name} successfully removed from the course`,
+                      )
+                      onRoleChange()
+                    })
+                    .catch(() => {
+                      message.error(
+                        `Failed to remove ${item.name} from the course`,
+                      )
+                    })
                 },
               })
             }}
-          >
-            {role !== Role.PROFESSOR ? (
-              <Menu.Item key={Role.PROFESSOR}>Professor</Menu.Item>
-            ) : null}
-            {role !== Role.TA ? (
-              <Menu.Item key={Role.TA}>Teaching Assistant</Menu.Item>
-            ) : null}
-            {role !== Role.STUDENT ? (
-              <Menu.Item key={Role.STUDENT}>Student</Menu.Item>
-            ) : null}
-          </Menu>
-        }
-        className="flex-grow-0"
-      >
-        <a className="ant-dropdown-link" onClick={(e) => e.preventDefault()}>
-          Change Role <DownOutlined />
-        </a>
-      </Dropdown>
-
-      {(role === Role.TA || role === Role.PROFESSOR) && (
-        // NOTE: if you modify this popover, you may also want to make changes to the popover in StaffList
-        <Popover
-          trigger="click"
-          overlayClassName="min-w-80"
-          content={
-            <div className="flex flex-col gap-y-2">
-              <TextArea
-                placeholder="Can answer questions about MATH 101, PHYS 102..."
-                autoSize={{ minRows: 4, maxRows: 7 }}
-                value={tempTaNotes}
-                onChange={(e) => {
-                  setCanSave(e.target.value !== item.TANotes)
-                  setTempTaNotes(e.target.value)
-                }}
-              />
-              <div className="flex items-center justify-start">
-                <Button
-                  disabled={!canSave}
-                  loading={saveLoading}
-                  onClick={async () => {
-                    setSaveLoading(true)
-                    await API.course
-                      .updateTANotes(courseId, item.id, tempTaNotes ?? '')
-                      .then(() => {
-                        setSaveSuccessful(true)
-                        setCanSave(false)
-                        item.TANotes = tempTaNotes
-                        // saved goes away after 1s
-                        setTimeout(() => {
-                          setSaveSuccessful(false)
-                        }, 1000)
-                      })
-                      .catch((e) => {
-                        const errorMessage = getErrorMessage(e)
-                        message.error(errorMessage)
-                      })
-                      .finally(() => {
-                        setSaveLoading(false)
-                      })
-                  }}
-                >
-                  Save
-                </Button>
-                <div>
-                  {
-                    <span
-                      className={`ml-2 text-green-500 transition-opacity duration-300 ${
-                        saveSuccessful ? 'opacity-100' : 'opacity-0'
-                      }`}
-                    >
-                      Saved!
-                    </span>
-                  }
-                </div>
-              </div>
-            </div>
-          }
-          title={
-            <div className="flex items-center">
-              <div>{item.name} - TA Notes</div>
-              <div>
-                <Tooltip title="Here you can set notes on your TAs (e.g. the types of questions a TA can answer). Other users can then hover the TA to see these notes. TAs are able to modify their own notes.">
-                  <span className="ml-2 text-gray-500">
-                    <QuestionCircleOutlined />
-                  </span>
-                </Tooltip>
-              </div>
-            </div>
-          }
-        >
-          <Button
-            icon={
-              item.TANotes ? (
-                <NotebookText className="p-[0.075rem] text-gray-700 transition-colors duration-200 ease-out hover:text-[#5ba1d4]" />
-              ) : (
-                <Notebook className="p-[0.075rem] transition-colors duration-200 ease-out hover:text-[#5ba1d4]" />
-              )
-            }
-            className="mx-2"
           />
-        </Popover>
-      )}
-
-      {userInfo.id !== item.id && (
-        <Button
-          icon={<UserDeleteOutlined />}
-          danger
-          className={role === Role.STUDENT ? 'ml-2' : ''}
-          onClick={() => {
-            Modal.confirm({
-              title: <div className="font-bold text-red-600">Warning</div>,
-              content: (
-                <div>
-                  You are about to <span className="text-red-600">remove</span>{' '}
-                  <span className="font-bold">{item.name}</span> from the
-                  course.
-                  <br />
-                  <br />
-                  Are you sure you want to proceed?
-                </div>
-              ),
-              okText: 'Yes',
-              okType: 'danger',
-              cancelText: 'No',
-
-              onOk() {
-                API.organizations
-                  .dropUserCourses(
-                    userInfo.organization?.orgId ?? -1,
-                    item.id,
-                    [courseId],
-                  )
-                  .then(() => {
-                    message.success(
-                      `${item.name} successfully removed from the course`,
-                    )
-                    onRoleChange()
-                  })
-                  .catch(() => {
-                    message.error(
-                      `Failed to remove ${item.name} from the course`,
-                    )
-                  })
-              },
-            })
-          }}
-        />
-      )}
+        )}
+      </Row>
     </List.Item>
   )
 }
