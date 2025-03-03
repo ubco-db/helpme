039048b876d2a86ae9fab5258c2f83371f6836ea
Adam Fipke <adamfipke@gmail.com>
2025-03-03 00:16:17 -0800

    forgot to update version on footerbar

--- packages/frontend/app/(dashboard)/components/FooterBar.tsx
+++ packages/frontend/app/(dashboard)/components/FooterBar.tsx
@@ -45,7 +45,7 @@ const FooterBar: React.FC = () => {
             IReadTheChangelog()
           }}
         >
-          Version 1.3.0{' '}
+          Version 1.4.0{' '}
           {userInfo.readChangeLog === false && (
             <span className="text-green-500">(New Changes!)</span>
           )}
