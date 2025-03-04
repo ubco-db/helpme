4ee7994fba428a4b1054e124590a93152c35bc85
Patrick Ma <patrickma6199@gmail.com>
2025-03-04 14:54:56 -0800

    Added integration test

--- packages/server/src/semester/semester.module.ts
+++ packages/server/src/semester/semester.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SemesterController } from './semester.controller';
-import { SemesterService } from './semester.service';
-import { ApplicationConfigModule } from '../config/application_config.module';
-
-// TODO: remove this service (semesters will be fetched with organization or course data so it's be in those controllers instead)
 
 @Module({
   controllers: [SemesterController],
-  providers: [SemesterService],
+  providers: [],
   imports: [],
 })
 export class SemesterModule {}
