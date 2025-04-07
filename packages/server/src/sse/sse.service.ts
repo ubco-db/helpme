e68391b550663ca9c5064a8f9fb3f68590837f92
Adam Fipke <adamfipke@gmail.com>
2025-04-07 14:34:08 -0700

    okay welp so anyways that didn't work. Cursor then came up with some BS solutions, tried mocking it (which i specified i didn't want multiple times), and at one point started straight up deleting tests claiming it was fixing 'linter errors'. I'm gonna just console log more details

--- packages/server/src/sse/sse.service.ts
+++ packages/server/src/sse/sse.service.ts
@@ -159,6 +159,9 @@ export class SSEService<T> implements OnModuleDestroy {
 
     const roomInfo = await redis.smembers(room).catch((err) => {
       console.error(ERROR_MESSAGES.sseService.roomMembers);
+      console.log(room);
+      console.log(payload.toString());
+      console.log(payload);
       console.error(err);
     });
 
