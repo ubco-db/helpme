d51f3e8cb9f0a610c39aaccf30cbd9659b630e8d
Adam Fipke <adamfipke@gmail.com>
2025-07-01 17:42:53 -0700

    Update packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/chatbot_settings/components/ChatbotSettingsModal.tsx
    Co-authored-by: Bridgette Hunt <112979899+bhunt02@users.noreply.github.com>

--- packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/chatbot_settings/components/ChatbotSettingsModal.tsx
+++ packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/chatbot_settings/components/ChatbotSettingsModal.tsx
@@ -286,7 +286,7 @@ const ChatbotSettingsModal: React.FC<ChatbotSettingsModalProps> = ({
         <Form.Item
           name="similarityThresholdDocuments"
           label={
-            <Tooltip title="Set the minimum similarity threshold when retrieving relevant information blocks. You can turn this up if you notice that the chatbot is grabbing irrelevant documents, or turn it down if it's not grabbing the chunks that it should have (generally, it's better to have this lower/default since it's better the AI have too much information than too little).">
+            <Tooltip title="Set the minimum similarity threshold when retrieving relevant information blocks. You can increase this if you notice that the chatbot is retrieving irrelevant documents, or decrease it if it's not grabbing the chunks that it should have. In general, this threshold should be left default or at a low value so the AI has more information to work with, rather than too little.">
               Similarity Threshold for Chunks <InfoCircleOutlined />
             </Tooltip>
           }
