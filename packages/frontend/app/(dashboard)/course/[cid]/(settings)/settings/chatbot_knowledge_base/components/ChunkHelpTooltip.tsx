6e28d95868c8c4e03551fe6ba6c520c8b661e715
Adam Fipke <adamfipke@gmail.com>
2025-07-03 11:45:09 -0700

    adjusted ChunkHelpTooltip a little more

--- packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/chatbot_knowledge_base/components/ChunkHelpTooltip.tsx
+++ packages/frontend/app/(dashboard)/course/[cid]/(settings)/settings/chatbot_knowledge_base/components/ChunkHelpTooltip.tsx
@@ -6,7 +6,7 @@ const ChunkHelpTooltip: React.FC = () => {
   return (
     <div className="text-gray-500">
       <Tooltip
-        title={`Chatbot document "chunks" essentially function as the chatbot's knowledge base and are what are actually cited by the chatbot. Each chunk can have a page number, which is the page number of the original document that this chunk came from. Note that modifying a chunk does not actually modify the original document nor does it affect any previously asked chatbot questions.`}
+        title={`Chatbot document "chunks" function as the knowledge base for the chatbot, where the most relevant chunks are sent into the LLM with the user's question to provide better answers. These retrieved chunks form the citations you see when asking the chatbot questions.\n\nEach chunk can have a page number, which is the page number of the original document that this chunk came from. Note that modifying a chunk does not actually modify the original document nor does it affect any previously asked chatbot questions.`}
       >
         Help <QuestionCircleOutlined />
       </Tooltip>
