616ce21a4d71cf36f49836955e3b5575126ccd4a
bhunt02 <bridgette.hunt02@outlook.com>
2025-07-19 14:12:16 -0700

    migration correction

--- packages/server/migration/1751851632788-async-question-anonymous-visible.ts
+++ packages/server/migration/1751851632788-async-question-anonymous-visible.ts
@@ -13,13 +13,17 @@ export class AsyncQuestionAnonymousVisible1751851632788
       `ALTER TABLE "async_question_model" ADD "authorSetVisible" boolean NOT NULL DEFAULT false`,
     );
     await queryRunner.query(
-      `ALTER TABLE "async_question_model" ADD "staffSetVisible" boolean NOT NULL DEFAULT false`,
+      `ALTER TABLE "async_question_model" ADD "staffSetVisible" boolean`,
     );
     await queryRunner.query(
       `ALTER TABLE "async_question_model" ADD "isAnonymous" boolean NOT NULL DEFAULT true`,
     );
     await queryRunner.query(
       `ALTER TABLE "course_settings_model" ADD "asyncCentreDefaultAnonymous" boolean NOT NULL DEFAULT true`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "course_settings_model" ADD "asyncCentreAuthorPublic" boolean NOT NULL DEFAULT false
+    `,
     );
     await queryRunner.query(
       `UPDATE "async_question_model" SET "staffSetVisible" = "visible"`,
@@ -38,6 +42,10 @@ export class AsyncQuestionAnonymousVisible1751851632788
     );
     await queryRunner.query(
       `ALTER TABLE "course_settings_model" DROP COLUMN "asyncCentreDefaultAnonymous"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "course_settings_model" DROP COLUMN "asyncCentreAuthorPublic"
+    `,
     );
     await queryRunner.query(
       `ALTER TABLE "async_question_model" DROP COLUMN "isAnonymous"`,
