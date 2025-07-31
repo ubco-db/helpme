d9c524241731fb50382c06e14b807a718cf15520
bhunt02 <bridgette.hunt02@outlook.com>
2025-07-30 23:50:44 -0700

    security vulnerabilities resolution, literal "resolutions" added to force old, unupdated packages to use newer impl of their dependencies (hopefully that goes fine)

---
+++ eslint.config.js
@@ -0,0 +1,71 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslintEslintPlugin = require("@typescript-eslint/eslint-plugin");
+const noOnlyTests = require("eslint-plugin-no-only-tests");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([
+    {
+        ignores: ['langchain-services/**/*'],
+        languageOptions: {
+            parser: tsParser,
+            sourceType: "module",
+
+            parserOptions: {
+                project: ["./packages/*/tsconfig.json"],
+                tsconfigRootDir: __dirname,
+            },
+
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
+
+        plugins: {
+            "@typescript-eslint": typescriptEslintEslintPlugin,
+            "no-only-tests": noOnlyTests,
+        },
+
+        extends: compat.extends(
+            "plugin:@typescript-eslint/eslint-recommended",
+            "plugin:@typescript-eslint/recommended",
+            "prettier",
+        ),
+
+        rules: {
+            "@typescript-eslint/interface-name-prefix": "off",
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "no-only-tests/no-only-tests": "error",
+
+            "@typescript-eslint/no-unused-vars": [1, {
+                varsIgnorePattern: "^_",
+            }],
+        },
+    },
+    globalIgnores(["**/.eslintrc.js", "./langchain-services/**/*"]),
+    {
+        files: ["**/*.entity.ts"],
+
+        rules: {
+            "@typescript-eslint/no-unused-vars": "off",
+        },
+    },
+    globalIgnores(["langchain-services/**/*"])
+]);
