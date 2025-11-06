8953f84a562222d04226808875d82265f1dc045d
Adam Fipke <adamfipke@gmail.com>
2025-11-06 15:03:44 -0800

    --amend

--- packages/server/ormconfig.ts
+++ packages/server/ormconfig.ts
@@ -52,6 +52,7 @@ import { ChatbotProviderModel } from './src/chatbot/chatbot-infrastructure-model
 import { CourseChatbotSettingsModel } from './src/chatbot/chatbot-infrastructure-models/course-chatbot-settings.entity';
 import { OrganizationChatbotSettingsModel } from './src/chatbot/chatbot-infrastructure-models/organization-chatbot-settings.entity';
 import { LLMTypeModel } from './src/chatbot/chatbot-infrastructure-models/llm-type.entity';
+import { ProfInviteModel } from './src/course/prof-invite.entity';
 // set .envs to their default values if the developer hasn't yet set them
 if (fs.existsSync('.env')) {
   config();
@@ -131,6 +132,7 @@ const typeorm: DataSourceOptions = {
     CourseChatbotSettingsModel,
     OrganizationChatbotSettingsModel,
     LLMTypeModel,
+    ProfInviteModel,
   ],
   logging:
     process.env.NODE_ENV !== 'production'
