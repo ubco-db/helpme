f86f6eca86b73964235614f8e255bb89bec269e8
kjassani <jassanikarim8@gmail.com>
2026-01-12 21:15:49 -0800

    exposing SWR error to allow handling unauthorized course access

--- packages/frontend/app/hooks/useCourse.tsx
+++ packages/frontend/app/hooks/useCourse.tsx
@@ -2,12 +2,22 @@ import { API } from '../api'
 import { GetCourseResponse } from '@koh/common'
 import useSWR from 'swr'
 
+export type GetCourseError = Error & {
+  response?: {
+    status: number
+    data: {
+      message: string
+    }
+  }
+}
+
 export function useCourse(cid: number | null): {
   course: GetCourseResponse | undefined
   mutateCourse: () => void
+  error: GetCourseError
 } {
   const key = cid ? `/api/v1/courses/${cid}` : null
-  const { data: course, mutate: mutateCourse } = useSWR(key, async () => {
+  const { data: course, mutate: mutateCourse, error } = useSWR(key, async () => {
     if (cid === null) {
       // this should never throw since the key will be null
       throw new Error('cid is somehow null in useCourse')
@@ -17,5 +27,6 @@ export function useCourse(cid: number | null): {
   return {
     course,
     mutateCourse,
+    error,
   }
 }
