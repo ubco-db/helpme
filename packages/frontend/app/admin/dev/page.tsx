66188103fb51ba5d6b6c30cec8f38eef1bbb253d
Adam Fipke <adamfipke@gmail.com>
2026-07-07 17:24:14 -0700

    admin dev tools: removed populateEmailSubscriptions and populateChatToken endpoints and buttons since they aren't needed anymore (I think they were implemented as a 1-time thing). Now you just write a query in a migration file

--- packages/frontend/app/admin/dev/page.tsx
+++ packages/frontend/app/admin/dev/page.tsx
@@ -7,7 +7,7 @@ import { getErrorMessage } from '@/app/utils/generalUtils'
 import { formatDateAndTimeForExcel } from '@/app/utils/timeFormatUtils'
 import { InfoCircleOutlined } from '@ant-design/icons'
 import { CronJob } from '@koh/common'
-import { Button, Card, message, Table, Tag, Tooltip } from 'antd'
+import { Button, Card, Divider, message, Table, Tag, Tooltip } from 'antd'
 import { useEffect, useState } from 'react'
 
 const dateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}[+-]\d{2}:\d{2}$/
@@ -47,39 +47,6 @@ const DevPage: React.FC = () => {
       message.error(json.message)
     }
   }
-  const populateTable = async () => {
-    const response = await fetch(
-      `/api/v1/organization/${userInfo.organization?.orgId}/populate_chat_token_table`,
-      {
-        method: 'POST',
-      },
-    )
-
-    const json = await response.json()
-
-    if (response.ok) {
-      message.success(json.message)
-    } else {
-      message.error(json.message)
-    }
-  }
-
-  const populateSubscriptions = async () => {
-    const response = await fetch(
-      `/api/v1/organization/${userInfo.organization?.orgId}/populate_subscription_table`,
-      {
-        method: 'POST',
-      },
-    )
-
-    const json = await response.json()
-
-    if (response.ok) {
-      message.success(json.message)
-    } else {
-      message.error(json.message)
-    }
-  }
 
   return (
     <Card>
@@ -89,46 +56,18 @@ const DevPage: React.FC = () => {
         </h2>
 
         <div className="mb-6">
-          <Tooltip title="Resets the daily chatbot limit for all users in the organization">
+          <Tooltip title="Resets the daily chatbot limit for all users in the organization (30 for regular users, 300 for org professors)">
             <h3 className="mr-2 font-semibold text-gray-700">
               Reset Chat Token Usage Limit
               <InfoCircleOutlined className="ml-2 text-gray-500" />
             </h3>
           </Tooltip>
-          <Button
-            type="primary"
-            onClick={resetChatUsageLimit}
-            className="transform rounded bg-blue-500 px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-blue-600"
-          >
+          <Button type="primary" onClick={resetChatUsageLimit}>
             Reset
           </Button>
         </div>
 
-        <div>
-          <h3 className="mb-2 block font-semibold text-gray-700">
-            Populate Chat Token Table
-          </h3>
-          <Button
-            type="primary"
-            onClick={populateTable}
-            className="transform rounded bg-green-500 px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-green-600"
-          >
-            Populate
-          </Button>
-        </div>
-
-        <div>
-          <h3 className="mb-2 block font-semibold text-gray-700">
-            Populate Email Subscriptions Table
-          </h3>
-          <Button
-            type="primary"
-            onClick={populateSubscriptions}
-            className="transform rounded bg-green-500 px-4 py-2 font-bold text-white transition duration-300 ease-in-out hover:scale-105 hover:bg-green-600"
-          >
-            Populate
-          </Button>
-        </div>
+        <Divider />
 
         <div>
           <h3 className="mb-2 block font-semibold text-gray-700">
