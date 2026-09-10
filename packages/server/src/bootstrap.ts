982aa5b2c581fdb0c9a1bb50380daa943678e982
Adam Fipke <adamfipke@gmail.com>
2026-09-10 16:19:14 -0700

    nestjs logging: made console.log/error/warn use nestjs's built in logger

--- packages/server/src/bootstrap.ts
+++ packages/server/src/bootstrap.ts
@@ -1,5 +1,5 @@
 import { isProd } from '@koh/common';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
 import * as morgan from 'morgan';
@@ -14,6 +14,11 @@ import helmet from 'helmet';
 import LtiMiddleware from './lti/lti.middleware';
 
 export async function bootstrap(hot: any): Promise<void> {
+  const logger = new Logger('System');
+  console.log = (...args) => logger.log(args.join(' '));
+  console.error = (...args) => logger.error(args.join(' '));
+  console.warn = (...args) => logger.warn(args.join(' '));
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
